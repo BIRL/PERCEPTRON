cf5b94ea5c8a91c0eb31b77e4a593e977bd1fdb1
CHIRAGH-II <safee.ullah@gmail.com>
2020-02-02 03:06:08 +0500

    Update PSTFilterCPU.cs

--- Code/PerceptronLocalService/PerceptronLocalService/Engine/PSTFilterCPU.cs
+++ Code/PerceptronLocalService/PerceptronLocalService/Engine/PSTFilterCPU.cs
@@ -12,21 +12,29 @@ namespace PerceptronLocalService.Engine
         public void ScoreProteinsByPst(List<PstTagList> pstList, List<ProteinDto> mwProt)  //mwProt ShortListed Candidate Proteins According to Mass Tolerance
         {
             //int COUNTME = 0;
-            for (int iterationOnProteinList = 0; iterationOnProteinList <= mwProt.Count - 1;iterationOnProteinList++)
+            for (int iterationOnProteinList = 0; iterationOnProteinList <= mwProt.Count - 1; iterationOnProteinList++)
             {
-                double score = 0;
-                for (int iteration = 0; iteration <= pstList.Count - 1; iteration++)
-                {
-                    int TagOccurrences = Regex.Matches(mwProt[iterationOnProteinList].Sequence, pstList[iteration].PstTags).Count;
-                    score += (pstList[iteration].PstErrorScore + pstList[iteration].PstFrequency) * TagOccurrences;
-                    //if (TagOccurrences != 0 && pstList[iteration].PstTags == "RHR")
-                    //    COUNTME += 1;
 
-                }
-                score = score / mwProt[iterationOnProteinList].Sequence.Length;
-                if (score > 1)
-                    score = 1;
-                mwProt[iterationOnProteinList].PstScore = score;
+                //if (mwProt[iterationOnProteinList].Header == "Q9BTM9")
+                //{
+
+
+
+                    double score = 0;
+                    for (int iteration = 0; iteration <= pstList.Count - 1; iteration++)
+                    {
+                        int TagOccurrences = Regex.Matches(mwProt[iterationOnProteinList].Sequence, pstList[iteration].PstTags).Count;
+                        score += (pstList[iteration].PstErrorScore + pstList[iteration].PstFrequency) * TagOccurrences;
+                        //if (TagOccurrences != 0 && pstList[iteration].PstTags == "RHR")
+                        //    COUNTME += 1;
+
+                    }
+                    score = score / mwProt[iterationOnProteinList].Sequence.Length;
+                    if (score > 1)
+                        score = 1;
+                    mwProt[iterationOnProteinList].PstScore = score;
+
+                //}
             }
         }
     }
