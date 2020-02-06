e6a8751795fcb803f21b51668b4f26a6f63c9687
CHIRAGH-II <safee.ullah@gmail.com>
2020-02-07 03:22:17 +0500

    Create BasicJobInfo.cs

---
+++ Code/PerceptronAPI/PerceptronAPI/Models/BasicJobInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PerceptronAPI.Models
+{
+    public class BasicJobInfo
+    {
+        public string UserId;
+        public string Title;
+
+
+        public BasicJobInfo()
+        {
+            UserId = "";
+            Title = "";
+        }
+
+        public BasicJobInfo(string cUserId, string cTitle)
+        {
+            UserId = cUserId;
+            Title = cTitle;
+        }
+
+    }
+}
