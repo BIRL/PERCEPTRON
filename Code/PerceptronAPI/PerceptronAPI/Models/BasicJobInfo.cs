fc03b62306f0d60da6728316a432ef75c1eabbc2
CHIRAGH-II <safee.ullah@gmail.com>
2020-04-27 23:38:20 +0500

    Update BasicJobInfo.cs

--- Code/PerceptronAPI/PerceptronAPI/Models/BasicJobInfo.cs
+++ Code/PerceptronAPI/PerceptronAPI/Models/BasicJobInfo.cs
@@ -8,19 +8,22 @@ namespace PerceptronAPI.Models
     public class BasicJobInfo
     {
         public string UserId;
+        public string EmailId;
         public string Title;
         public int GuestEnabled;
 
         public BasicJobInfo()
         {
             UserId = "";
+            EmailId = "";
             Title = "";
-            GuestEnabled = 0; //Initializing as Not as Guest
+            GuestEnabled = 0; //Initializing Not as Guest
         }
 
-        public BasicJobInfo(string cUserId, string cTitle, int cGuestEnabled)
+        public BasicJobInfo(string cUserId,  string cEmailId, string cTitle, int cGuestEnabled)
         {
             UserId = cUserId;
+            EmailId = cEmailId;
             Title = cTitle;
             GuestEnabled = cGuestEnabled;
         }
