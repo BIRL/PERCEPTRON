5c869e2f03cb1b732facbc2984204763de77083f
CHIRAGH-II <safee.ullah@gmail.com>
2020-04-30 22:48:58 +0500

    Update

--- Code/PerceptronAPI/PerceptronAPI/Controllers/SearchController.cs
+++ Code/PerceptronAPI/PerceptronAPI/Controllers/SearchController.cs
@@ -234,6 +234,7 @@ namespace PerceptronAPI.Controllers
                 //}  //I'M COMMENTED
 
                 mm.IsBodyHtml = true;
+                var networkCred = new NetworkCredential("perceptron@lums.edu.pk", "*****");
                 var smtp = new SmtpClient
                 {
                     Host = "smtp.office365.com",
