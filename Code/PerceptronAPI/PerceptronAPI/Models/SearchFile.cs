f13baf1bd973f78e58fd2b0496ebcf04accc5d01
CHIRAGH-II <safee.ullah@gmail.com>
2020-07-02 22:50:43 +0500

    Update SearchFile.cs

--- Code/PerceptronAPI/PerceptronAPI/Models/SearchFile.cs
+++ Code/PerceptronAPI/PerceptronAPI/Models/SearchFile.cs
@@ -19,5 +19,6 @@ namespace PerceptronAPI.Models
         public string FileName { get; set; }
         public string FileType { get; set; }
         public string UniqueFileName { get; set; }
+        public string FileUniqueId { get; set; }
     }
 }
