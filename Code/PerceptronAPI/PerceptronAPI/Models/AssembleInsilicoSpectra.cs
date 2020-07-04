9c2004d004edaa8eab73fa1e9c8355bf8251afd2
CHIRAGH-II <safee.ullah@gmail.com>
2020-07-04 17:33:54 +0500

    Update AssembleInsilicoSpectra.cs

--- Code/PerceptronAPI/PerceptronAPI/Models/AssembleInsilicoSpectra.cs
+++ Code/PerceptronAPI/PerceptronAPI/Models/AssembleInsilicoSpectra.cs
@@ -7,38 +7,38 @@ namespace PerceptronAPI.Models
 {
     public class AssembleInsilicoSpectra
     {
-        public int MatchedIndex;
-        public int PeakIndex;
-        public string IonType;
-        public double Theoretical_mz;
-        public double Experimental_mz;
+        public List<int> ListIndices;
+        public List<string> ListFragIon;
+        public List<double> ListExperimental_mz;
+        public List<double> ListTheoretical_mz;
+        public List<double> ListAbsError;
 
-        public AssembleInsilicoSpectra(int cMatchedIndex, int cPeakIndex, string cIonType, double cTheoretical_mz, double cExperimental_mz)
+        public AssembleInsilicoSpectra(List<int> cListIndices, List<string> cListFragIon, List<double> cListExperimental_mz, List<double> cListTheoretical_mz, List<double> cListAbsError)
         {
-            MatchedIndex = cMatchedIndex;
-            PeakIndex = cPeakIndex;
-            IonType = cIonType;
-            Theoretical_mz = cTheoretical_mz;
-            Experimental_mz = cExperimental_mz;
+            ListIndices = cListIndices;
+            ListFragIon = cListFragIon;
+            ListExperimental_mz = cListExperimental_mz;
+            ListTheoretical_mz = cListTheoretical_mz;
+            ListAbsError = cListAbsError;
         }
-
-
-
     }
 }
 
 
-//public int MatchedIndex;
-// public int PeakIndex;
-// public List<string> IonType;
-// public List<double> Theoretical_mz;
-// public List<double> Experimental_mz;
 
-// public AssembleInsilicoSpectra(List<int> cMatchedIndex, List<int> cPeakIndex, List<string> cIonType, List<double> cTheoretical_mz, List<double> cExperimental_mz)
-// {
-//     MatchedIndex = cMatchedIndex;
-//     PeakIndex = cPeakIndex;
-//     IonType = cIonType;
-//     Theoretical_mz = cTheoretical_mz;
-//     Experimental_mz = cExperimental_mz;
-// }
+
+
+//public int MatchedIndex;
+//        public int PeakIndex;
+//        public string IonType;
+//        public double Theoretical_mz;
+//        public double Experimental_mz;
+
+//        public AssembleInsilicoSpectra(int cMatchedIndex, int cPeakIndex, string cIonType, double cTheoretical_mz, double cExperimental_mz)
+//        {
+//            MatchedIndex = cMatchedIndex;
+//            PeakIndex = cPeakIndex;
+//            IonType = cIonType;
+//            Theoretical_mz = cTheoretical_mz;
+//            Experimental_mz = cExperimental_mz;
+//        }
