18d64df05dec9f9444567ef7c7a6428afc0e6152
CHIRAGH-II <safee.ullah@gmail.com>
2020-07-25 23:52:31 +0500

    Update ModificationMWShift.cs

--- Code/PerceptronLocalService/PerceptronLocalService/Utility/ModificationMWShift.cs
+++ Code/PerceptronLocalService/PerceptronLocalService/Utility/ModificationMWShift.cs
@@ -11,75 +11,65 @@ namespace PerceptronLocalService.Utility
 
         public double ModificationMWShiftTable(string Mod) //Gives the weight of different types of modifications
         {
+            double ModWeight = 0.0;
 
-            //SHIFTed From PostTranslationalModificationCpu.cs with New name (Previouly its ModTable) 
-            //TO THE UTILITY FOR COMBINE USE
-            // DELETE ModTable FROM PostTranslationalModificationCpu.cs AFTER STABLILITY.
-            // NO MORE EDITTING THERE
-
-            //#Enhancement Switch Cases will be more better here
-            double ModWeight = 0;
-            if (Mod == "Propionamidation" || Mod == "Cys_PAM")
-                ModWeight = 174.04631;
-            else if (Mod == "Pyruvate-S")
-                ModWeight = -17.0265;
-            else if (Mod == "Pyruvate-C")
-                ModWeight = 70.0055;
-            else if (Mod == "Amidation")  //Site = 'F'
-                ModWeight = -0.984016;
-            else if (Mod == "Citrullination")
-                ModWeight = 0.984016;
-            else if (Mod == "Methylation")  //Site = 'R' || 'K'
-                ModWeight = 14.0156;
-            else if (Mod == "Hydroxylation")  //Site = 'P'
-                ModWeight = 15.9949;
-            else if (Mod == "Sulfoxide" || Mod == "MSO")  //Site = 'M'
-                ModWeight = 147.0354;
-            else if (Mod == "Formylation")
-                ModWeight = 27.9949;
-            else if (Mod == "DiMethylation")
-                ModWeight = 28.0313;
-            else if (Mod == "S-Nitrosylation")
-                ModWeight = 28.9902;
-            else if (Mod == "Sulfone")
-                ModWeight = 31.9898;    // 32.00
-            else if (Mod == "DiHydroxylation")
-                ModWeight = 31.9898;
-            else if (Mod == "TriMethylation")
-                ModWeight = 42.047;
-            else if (Mod == "Acetylation")  //Site = 'S' || 'M' || 'K'  || 'A'
-                ModWeight = 42.0106;
-            else if (Mod == "Gamma-Carboxyglutamic-Acid")
-                ModWeight = 43.9898;
-            else if (Mod == "Nitration")
-                ModWeight = 44.9851;
-            else if (Mod == "Phosphorylation")  //Site = 'Y'
-                ModWeight = 79.9663;
-            else if (Mod == "Pyrrolidone-Aarboxylic-Acid")
-                ModWeight = -17.0265;
-            else if (Mod == "O-linked-Glycosylation")  //Site = 'S'
-                ModWeight = 203.0794;
-            else if (Mod == "Palmitoylation")
-                ModWeight = 238.23;
-            else if (Mod == "Glutathionylation")
-                ModWeight = 305.068;
-            else if (Mod == "N-linked-Glycosylation")
-                ModWeight = 317.122;
-
-            //New Added
-            else if (Mod == "Carboxymethylation")  //Site = 'C'
-                ModWeight = 161.01466;
-
-            else if (Mod == "Carboxyamidomethylation")  //Site = 'C'
-                ModWeight = 160.03065;
-            else if (Mod == "Pyridylethylation")  //Site = 'C'
-                ModWeight = 208.067039;
-
-
-            else
-                ModWeight = 0.0;
-            return ModWeight;
+            switch (Mod)
+            {
+                case "Propionamidation":
+                case "Cys_PAM":
+                    return ModWeight = 174.04631;
+                case "Pyruvate-S":
+                    return ModWeight = -17.0265;
+                case "Pyruvate-C":
+                    return ModWeight = 70.0055;
+                case "Amidation":         //Site = 'F'
+                    return ModWeight = -0.984016;
+                case "Citrullination":
+                    return ModWeight = 0.984016;
+                case "Methylation":           //Site = 'R' || 'K'
+                    return ModWeight = 14.0156;
+                case "Hydroxylation":         //Site = 'P'
+                    return ModWeight = 15.9949;
+                case "Sulfoxide":
+                case "MSO":                   //Site = 'M'
+                    return ModWeight = 15.9949;
+                case "Formylation":
+                    return ModWeight = 27.9949;
+                case "DiMethylation":
+                    return ModWeight = 28.0313;
+                case "S-Nitrosylation":
+                    return ModWeight = 28.9902;
+                case "Sulfone":
+                    return ModWeight = 31.9898;
+                case "DiHydroxylation":
+                    return ModWeight = 31.9898;
+                case "TriMethylation":
+                    return ModWeight = 42.047;
+                case "Acetylation":       //Site = 'S' || 'M' || 'K'  || 'A'
+                    return ModWeight = 42.0106;
+                case "Gamma-Carboxyglutamic-Acid":
+                    return ModWeight = 43.9898;
+                case "Nitration":
+                    return ModWeight = 44.9851;
+                case "Phosphorylation":       //Site = 'Y'
+                    return ModWeight = 79.9663;
+                case "Pyrrolidone-Aarboxylic-Acid":
+                    return ModWeight = -17.0265;
+                case "O-linked-Glycosylation":    //Site = 'S'
+                    return ModWeight = 203.0794;
+                case "Palmitoylation":        //Site = 'Y'
+                    return ModWeight = 238.23;
+                case "Glutathionylation":         //Site = 'C'
+                    return ModWeight = 305.068;
+                case "N-linked-Glycosylation":
+                    return ModWeight = 317.122;
+                case "Carboxymethylation":        //Site = 'C'
+                    return ModWeight = 160.03065;
+                case "Pyridylethylation":         //Site = 'C'
+                    return ModWeight = 208.067039;
+                default:
+                    return ModWeight;
+            }
         }
-
     }
 }
