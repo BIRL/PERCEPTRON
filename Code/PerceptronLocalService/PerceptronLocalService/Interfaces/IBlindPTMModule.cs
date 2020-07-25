85cef54af6b08e7041450b6b6d989f8cfee4ae0e
CHIRAGH-II <safee.ullah@gmail.com>
2020-07-25 15:42:38 +0500

    Create IBlindPTMModule.cs

---
+++ Code/PerceptronLocalService/PerceptronLocalService/Interfaces/IBlindPTMModule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PerceptronLocalService.DTO;
+
+namespace PerceptronLocalService.Interfaces
+{
+    public interface IBlindPTMModule
+    {
+        BlindPTMDto BlindPTMExtraction(List<newMsPeaksDto> peakData2DList, SearchParametersDto parameters);
+        List<ProteinDto> BlindPTMGeneral(List<ProteinDto> CandidateProtList, List<newMsPeaksDto> peakData2DList, double UserHopThreshold, BlindPTMDto BlindPTMExtractionInfo, SearchParametersDto parameters, string TypeOfFunction);
+        List<ProteinDto> BlindPTMLocalization(List<ProteinDto> Matches, List<newMsPeaksDto> peakData2DList, SearchParametersDto parameters);
+    }
+}
