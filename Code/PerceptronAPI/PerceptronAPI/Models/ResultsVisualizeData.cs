cb9540c7960cd3861ac19d0b23407bff1c4177b1
CHIRAGH-II <safee.ullah@gmail.com>
2020-07-26 14:39:24 +0500

    Update ResultsVisualizeData.cs

--- Code/PerceptronAPI/PerceptronAPI/Models/ResultsVisualizeData.cs
+++ Code/PerceptronAPI/PerceptronAPI/Models/ResultsVisualizeData.cs
@@ -3,18 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 
+using System.IO;
+
 namespace PerceptronAPI.Models
 {
     public class ResultsVisualizeData
     {
         public string QueryId;
-        public string NameOfFileWithPath;
+        public byte[] blob;
+        //public string NameOfFileWithPath;
         public AssembleInsilicoSpectra InsilicoSpectra;
 
-        public ResultsVisualizeData(string cQueryId, string cNameOfFileWithPath, AssembleInsilicoSpectra cInsilicoSpectra)
+        public ResultsVisualizeData(string cQueryId, byte[] cblob, AssembleInsilicoSpectra cInsilicoSpectra)
         {
             QueryId = cQueryId;
-            NameOfFileWithPath = cNameOfFileWithPath;
+            blob = cblob;
+            //NameOfFileWithPath = cNameOfFileWithPath;  //20200715  -- NameofFile
             InsilicoSpectra = cInsilicoSpectra;
         }
     }
