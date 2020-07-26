e63d9f4a0b830771ee0cd0973bc893df6070e1ad
CHIRAGH-II <safee.ullah@gmail.com>
2020-07-26 14:39:38 +0500

    Update SearchParameter.cs

--- Code/PerceptronAPI/PerceptronAPI/Models/SearchParameter.cs
+++ Code/PerceptronAPI/PerceptronAPI/Models/SearchParameter.cs
@@ -17,9 +17,11 @@ namespace PerceptronAPI.Models
         public string QueryId { get; set; }
         public string UserId { get; set; }
         public string Title { get; set; }
-        public string ProtDb { get; set; }
+        public string EmailId { get; set; }
+        public string ProteinDatabase { get; set; }
+        public int NumberOfOutputs { get; set; }
         public string InsilicoFragType { get; set; }
-        public int FilterDb { get; set; }
+        public string FilterDb { get; set; }
         public double PtmTolerance { get; set; }
         public int MinimumPstLength { get; set; }
         public int MaximumPstLength { get; set; }
@@ -27,25 +29,22 @@ namespace PerceptronAPI.Models
         public string MwTolUnit { get; set; }
         public double HopThreshhold { get; set; }
         public string HopTolUnit { get; set; }
-        public double GuiMass { get; set; }
-        public int Autotune { get; set; }
+        public string Autotune { get; set; }
         public string HandleIons { get; set; }
         public double MwSweight { get; set; }
         public double PstSweight { get; set; }
         public double InsilicoSweight { get; set; }
-        public int NumberOfOutputs { get; set; }
-        public int DenovoAllow { get; set; }
-        public int PtmAllow { get; set; }
+        public string DenovoAllow { get; set; }
+        public string PtmAllow { get; set; }
         public double NeutralLoss { get; set; }
         public double PSTTolerance { get; set; }
         public double PeptideTolerance { get; set; }
         public string PeptideToleranceUnit { get; set; }
-        public int Truncation { get; set; }
+        public string Truncation { get; set; }
         public string TerminalModification { get; set; }
         public double SliderValue { get; set; }
         public string CysteineChemicalModification { get; set; }
         public string MethionineChemicalModification { get; set; }
-        public string EmailId { get; set; }
         public int TruncationIndex { get; set; }
     }
 }
