6b0cf253d2f0038f1ff89aeaf7efc1571c00d0ea
CHIRAGH-II <safee.ullah@gmail.com>
2020-07-30 02:13:28 +0500

    Create Cys_CAM_CPU.cs

---
+++ Code/PerceptronLocalService/PerceptronLocalService/Utility/Cys_CAM_CPU.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PerceptronLocalService.DTO;
+
+namespace PerceptronLocalService.Utility
+{
+    class Cys_CAM_CPU
+    {
+        ModificationMWShift ModificationTableClass = new ModificationMWShift();
+
+        public List<PostTranslationModificationsSiteDto> Cys_CAM(string proteinSequence, double ptmTolerance)
+        {
+            var modName = "Carboxyamidomethylation";
+            double modWeight = ModificationTableClass.ModificationMWShiftTable(modName);
+            var site = 'C';
+
+            //SetsiteDetect(site);
+
+            // variable score is initialized
+            double score = 0;
+
+            // Variable to store total number of Lysine
+            var totalCys = 0;
+
+            // list "array" creation
+            var array = new List<PostTranslationModificationsSiteDto>();
+
+            // for loop run for as many times as there are characters(Amino Acids) in the protein sequence
+            for (var i = 0; i < proteinSequence.Length; i++)
+            {
+                if (proteinSequence[i] == 'C')
+                {
+                    var AminoAcid = new List<char>();
+                    totalCys = totalCys + 1;
+                    score = 1;
+                    AminoAcid.Add(proteinSequence[i]);  /// no need
+
+                    array.Add(new PostTranslationModificationsSiteDto(i, score, modWeight, modName, site, AminoAcid));
+                }
+
+            }
+            return array;
+        }
+    }
+}
