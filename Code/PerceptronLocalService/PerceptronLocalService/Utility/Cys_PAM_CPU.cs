7d179ceff6e7b72ef41a4f2bbd03b046b12ba690
CHIRAGH-II <safee.ullah@gmail.com>
2020-07-30 02:13:22 +0500

    Create Cys_PAM_CPU.cs

---
+++ Code/PerceptronLocalService/PerceptronLocalService/Utility/Cys_PAM_CPU.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PerceptronLocalService.DTO;
+
+namespace PerceptronLocalService.Utility
+{
+    class Cys_PAM_CPU
+    {
+        ModificationMWShift ModificationTableClass = new ModificationMWShift();
+
+        public List<PostTranslationModificationsSiteDto> Cys_PAM(string proteinSequence, double ptmTolerance)
+        {
+            var modName = "Propionamidation";
+            double modWeight = ModificationTableClass.ModificationMWShiftTable(modName);
+            var site = 'C';
+
+            // variable score is initialized
+            double score = 0;
+
+            // Variable to store total number of Lysine
+            var totalCys = 0;
+
+            // list "array" creation
+            var array = new List<PostTranslationModificationsSiteDto>();
+
+            // stores the amino acids found
+            var sub_sequence = new List<char>();
+
+            // for loop run for as many times as there are characters in the protein sequence
+            for (var i = 0; i < proteinSequence.Length; i++)
+            {
+                if (proteinSequence[i] == 'C')
+                {
+                    totalCys = totalCys + 1;
+                    score = 1;
+                    sub_sequence.Add(proteinSequence[i]);
+                }
+                array.Add(new PostTranslationModificationsSiteDto(i, score, modWeight, modName, site, sub_sequence));
+            }
+            return array;
+        }
+    }
+}
