85a5e1467b019fd3a598c8d36783b9b4b1f08b11
CHIRAGH-II <safee.ullah@gmail.com>
2020-08-07 02:59:46 +0500

    Create file-download.service.ts

---
+++ Code/PerceptronFrontEnd/src/app/file-download.service.ts
@@ -0,0 +1,14 @@
+// import { Injectable } from '@angular/core';
+// import {HttpResponse} from '@angular/common/http';
+// import {Http, ResponseContentType} from '@angular/http';
+// import {Observable} from 'rxjs';
+
+
+// @Injectable({ providedIn: 'root' })
+// export class FileDownloadService {
+
+//   constructor(private http: Http) {}
+
+
+   
+// }
