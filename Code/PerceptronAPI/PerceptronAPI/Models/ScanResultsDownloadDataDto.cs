cc1423a0b07c962795ce27697876447a7415ef73
CHIRAGH-II <safee.ullah@gmail.com>
2020-08-10 06:31:35 +0500

    Create ScanResultsDownloadDataDto.cs

---
+++ Code/PerceptronAPI/PerceptronAPI/Models/ScanResultsDownloadDataDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PerceptronAPI.Models
+{
+    public class ScanResultsDownloadDataDto
+    {
+
+        public List<string> FileUniqueIdsList = new List<string>();
+        public List<string> FileNamesList = new List<string>();
+        public List<SearchResult> ListOfSearchResults = new List<SearchResult>();
+        public SearchParameter searchParameters;
+        public List<ResultPtmSite> PtmSites = new List<ResultPtmSite>();
+
+
+        public ScanResultsDownloadDataDto()
+        {
+
+        }
+
+        public ScanResultsDownloadDataDto(List<string> cFileUniqueIdsList, List<string> cFileNamesList, List<SearchResult> cListOfSearchResults,
+            SearchParameter csearchParameters, List<ResultPtmSite> cPtmSites)
+        {
+
+            FileUniqueIdsList = cFileUniqueIdsList;
+            FileNamesList = cFileNamesList;
+            ListOfSearchResults = cListOfSearchResults;
+            searchParameters = csearchParameters;
+            PtmSites = cPtmSites;
+        }
+    }
+}
