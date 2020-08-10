3b55b4fec1c4927ea624cf35337dea61f4491ce3
CHIRAGH-II <safee.ullah@gmail.com>
2020-08-10 06:29:12 +0500

    Create ResultsDownloadDto.cs

---
+++ Code/PerceptronAPI/PerceptronAPI/Models/ResultsDownloadDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PerceptronAPI.Models
+{
+    public class ResultsDownloadDto
+    {
+        public List<string> AllResultFilesNames;
+        public List<byte[]> ListOfFileBlobs;
+        
+        public ResultsDownloadDto(List<string> cAllResultFilesNames, List<byte[]> cListOfFileBlobs)
+        {
+            AllResultFilesNames = cAllResultFilesNames;
+            ListOfFileBlobs = cListOfFileBlobs;
+        }
+    }
+}
