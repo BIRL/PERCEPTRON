adefd1d3b67d6a965501d1d0b42cf0cdd0ec791e
CHIRAGH-II <safee.ullah@gmail.com>
2020-08-10 06:26:59 +0500

    Create BlindPtmInfo.cs

---
+++ Code/PerceptronAPI/PerceptronAPI/Models/BlindPtmInfo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace PerceptronAPI.Models
+{
+    public class BlindPtmInfo
+    {
+        public int BlindPtmLocalizationStart;
+        public double BlindPtmLocalizationMass;
+        public int BlindPtmLocalizationEnd;
+
+        public BlindPtmInfo(string BlindPtmInfo)
+        {
+            var BlindPtmLocalizationInfo = BlindPtmInfo.Split(',').ToList<string>();
+            var BlindPtmLocalizationStart = Convert.ToInt16(BlindPtmLocalizationInfo[0]);
+            var BlindPtmLocalizationMass = Convert.ToDouble(BlindPtmLocalizationInfo[1]);
+            var BlindPtmLocalizationEnd = Convert.ToInt16(BlindPtmLocalizationInfo[2]);
+        }
+    }
+}
