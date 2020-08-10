2238c04ebe374d7d5b6a502cda57bb034678a00b
CHIRAGH-II <safee.ullah@gmail.com>
2020-08-11 00:53:37 +0500

    Update PostTranslationModificationsSiteDto.cs

--- Code/PerceptronAPI/PerceptronAPI/Models/PostTranslationModificationsSiteDto.cs
+++ Code/PerceptronAPI/PerceptronAPI/Models/PostTranslationModificationsSiteDto.cs
@@ -9,15 +9,60 @@ namespace PerceptronAPI.Models
     {
 
         // Declaring Variables
+        public string stringSiteIndex;
+        public string stringModName;
+        public string stringSite;
+
+        public int SiteIndex;
+        public string ModName;
+        public string Site;
+
         public List<int> ListSiteIndex = new List<int>();   // amino acid Indices
         public List<string> ListModName = new List<string>();
         public List<string> ListSite = new List<string>();
+
+        //public PostTranslationModificationsSiteDto()
+        //{
+        //    // Initializing Variales
+        //    stringSiteIndex = "";
+        //    stringModName = "";
+        //    stringSite = "";
+        //}
+
+        public PostTranslationModificationsSiteDto()
+        {
+            // Initializing Variales
+            SiteIndex = 0;
+            ModName = "";
+            Site = "";
+        }
+
+
+        public PostTranslationModificationsSiteDto(int cSiteIndex, string cModName, string cSite)
+        {
+            // Initializing Variales
+            SiteIndex = cSiteIndex;
+            ModName = cModName;
+            Site = cSite;
+        }
 
         public PostTranslationModificationsSiteDto(ResultPtmSite PtmSites)
         {
             ListSiteIndex = PtmSites.Index.Split(',').Select(int.Parse).ToList();//PtmSites.Index.Split(',').ToList<string>(); //List<int>();
             ListModName = PtmSites.ModName.Split(',').ToList();
             ListSite = PtmSites.Site.Split(',').ToList();
+        }
+
+
+        public List<PostTranslationModificationsSiteDto> ProcessPtmSiteInfo(PostTranslationModificationsSiteDto cPtmSitesInfo)  // Just using to format the into required form
+        {
+            List<PostTranslationModificationsSiteDto> PtmSites = new List<PostTranslationModificationsSiteDto>();
+
+            for (int i = 0; i < cPtmSitesInfo.ListSite.Count; i++)
+            {
+                PtmSites.Add(new PostTranslationModificationsSiteDto(cPtmSitesInfo.ListSiteIndex[i], cPtmSitesInfo.ListModName[i], cPtmSitesInfo.ListSite[i]));
+            }
+            return PtmSites;
         }
     }
 }
