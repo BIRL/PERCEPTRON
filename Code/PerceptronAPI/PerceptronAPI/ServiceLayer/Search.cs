33067a693f91bd31887cd533dd44066b9a5c1fae
CHIRAGH-II <safee.ullah@gmail.com>
2020-08-30 03:09:18 +0500

    Depreciated

--- Code/PerceptronAPI/PerceptronAPI/ServiceLayer/Search.cs
+++ Code/PerceptronAPI/PerceptronAPI/ServiceLayer/Search.cs
@@ -1,74 +1,74 @@
-﻿using PerceptronAPI.Models;
-using PerceptronAPI.Repository;
+﻿//using PerceptronAPI.Models;
+//using PerceptronAPI.Repository;
 
-namespace PerceptronAPI.ServiceLayer
-{
-    public class Search
-    {
-        static MySqlDatabase Db = new MySqlDatabase();
+//namespace PerceptronAPI.ServiceLayer
+//{
+//    public class Search
+//    {
+//        static MySqlDatabase Db = new MySqlDatabase();
         
-        /// <summary>
-        /// UseSQLDB.
-        /// Assign  MIssing params
-        /// SQLDB -> StoreSearchParameters
-        /// </summary>
-        /// <param name="parameters"></param>
-        /// <returns></returns>
+//        /// <summary>
+//        /// UseSQLDB.
+//        /// Assign  MIssing params
+//        /// SQLDB -> StoreSearchParameters
+//        /// </summary>
+//        /// <param name="parameters"></param>
+//        /// <returns></returns>
 
 
-        public static string ProteinSearch(SearchParametersDto parameters)
-        {
-            //var newJob = new Job
-            //{
-            //    Qid = parameters.Queryid,
-            //    Uid = parameters.UserId,
-            //    Progress = "Initiating Search!"
-            ////};
-            //CurrentJobs.Initialize();
-            //CurrentJobs.current.Add(newJob);
-            //int q = Db.QueryStore(parameters);
-            //if (q == -1)
-            //    return "Not ok";
-            SqlDatabase p=new SqlDatabase();
-            p.StoreSearchParameters(parameters);
-            return "ok";
-        }
+//        public static string ProteinSearch(SearchParametersDto parameters)
+//        {
+//            //var newJob = new Job
+//            //{
+//            //    Qid = parameters.Queryid,
+//            //    Uid = parameters.UserId,
+//            //    Progress = "Initiating Search!"
+//            ////};
+//            //CurrentJobs.Initialize();
+//            //CurrentJobs.current.Add(newJob);
+//            //int q = Db.QueryStore(parameters);
+//            //if (q == -1)
+//            //    return "Not ok";
+//            SqlDatabase p=new SqlDatabase();
+//            p.StoreSearchParameters(parameters);
+//            return "ok";
+//        }
 
-        public static string ProteinSearch1(SearchParametersDto parameters)
-        {
-            //var newJob = new Job
-            //{
-            //    Qid = parameters.Queryid,
-            //    Uid = parameters.UserId,
-            //    Progress = "Initiating Search!"
-            ////};
-            //CurrentJobs.Initialize();
-            //CurrentJobs.current.Add(newJob);
-            //int q = Db.QueryStore(parameters);
-            //if (q == -1)
-            //    return "Not ok";
-            SqlDatabase p = new SqlDatabase();
-            p.StoreSearchFiles(parameters);
-            return "ok";
-        }
+//        public static string ProteinSearch1(SearchParametersDto parameters)
+//        {
+//            //var newJob = new Job
+//            //{
+//            //    Qid = parameters.Queryid,
+//            //    Uid = parameters.UserId,
+//            //    Progress = "Initiating Search!"
+//            ////};
+//            //CurrentJobs.Initialize();
+//            //CurrentJobs.current.Add(newJob);
+//            //int q = Db.QueryStore(parameters);
+//            //if (q == -1)
+//            //    return "Not ok";
+//            SqlDatabase p = new SqlDatabase();
+//            p.StoreSearchFiles(parameters);
+//            return "ok";
+//        }
 
 
-        public static string Progress_reporter(string qid)
-        {
-            // DBConnect db = new DBConnect();
-            //return db.Get_Progress(qid).ToString();
-            //string pg = "-1";
-            //if (CurrentJobs.current != null)
-            //{
-            //    for (int i = 0; i < CurrentJobs.current.Count; i++)
-            //    {
-            //        if (CurrentJobs.current[i].qid == qid)
-            //            pg = CurrentJobs.current[i].progress;
-            //        if (pg == "Done")
-            //            CurrentJobs.current.RemoveAt(i);
-            //    }
-            //}
-            return "1";
-        }
-    }
-}
+//        public static string Progress_reporter(string qid)
+//        {
+//            // DBConnect db = new DBConnect();
+//            //return db.Get_Progress(qid).ToString();
+//            //string pg = "-1";
+//            //if (CurrentJobs.current != null)
+//            //{
+//            //    for (int i = 0; i < CurrentJobs.current.Count; i++)
+//            //    {
+//            //        if (CurrentJobs.current[i].qid == qid)
+//            //            pg = CurrentJobs.current[i].progress;
+//            //        if (pg == "Done")
+//            //            CurrentJobs.current.RemoveAt(i);
+//            //    }
+//            //}
+//            return "1";
+//        }
+//    }
+//}
--- Code/PerceptronAPI/PerceptronAPI/ServiceLayer/Users.cs
+++ Code/PerceptronAPI/PerceptronAPI/ServiceLayer/Users.cs
@@ -1,71 +1,71 @@
-﻿using System;
-using System.Collections.Generic;
-using PerceptronAPI.Models;
-using PerceptronAPI.Repository;
+﻿//using System;
+//using System.Collections.Generic;
+//using PerceptronAPI.Models;
+//using PerceptronAPI.Repository;
 
-namespace PerceptronAPI.ServiceLayer
-{
-    public class Users
-    {
-        public string NewUser(string uName, string name, string email, string rPass)
-        {
-            var db = new MySqlDatabase();
-            var g = Guid.NewGuid();
-            return db.Registeration(uName, rPass, name, email, g.ToString());
-        }
+//namespace PerceptronAPI.ServiceLayer
+//{
+//    public class Users
+//    {
+//        public string NewUser(string uName, string name, string email, string rPass)
+//        {
+//            var db = new MySqlDatabase();
+//            var g = Guid.NewGuid();
+//            return db.Registeration(uName, rPass, name, email, g.ToString());
+//        }
 
-        public string Login(string id, string pass, string g)
-        {
-            var db = new MySqlDatabase();
-            return db.Login(id, pass, g);
-        }
+//        public string Login(string id, string pass, string g)
+//        {
+//            var db = new MySqlDatabase();
+//            return db.Login(id, pass, g);
+//        }
 
-        public bool Activate(string email)
-        {
-            var db = new MySqlDatabase();
-            return db.Activate(email);
-        }
+//        public bool Activate(string email)
+//        {
+//            var db = new MySqlDatabase();
+//            return db.Activate(email);
+//        }
 
-        public bool SessionCheck(string id, string g)
-        {
-            var db = new MySqlDatabase();
-            return db.Session_validator(id, g);
-        }
+//        public bool SessionCheck(string id, string g)
+//        {
+//            var db = new MySqlDatabase();
+//            return db.Session_validator(id, g);
+//        }
 
-        public bool Logout(string id, string g)
-        {
-            var db = new MySqlDatabase();
-            return db.Logout(id, g);
-        }
+//        public bool Logout(string id, string g)
+//        {
+//            var db = new MySqlDatabase();
+//            return db.Logout(id, g);
+//        }
 
-        public bool UpdateInfo(string id, string newName, string newEmail)
-        {
-            var db = new MySqlDatabase();
-            return db.UpdateInfo(newEmail, id, newName);
-        }
+//        public bool UpdateInfo(string id, string newName, string newEmail)
+//        {
+//            var db = new MySqlDatabase();
+//            return db.UpdateInfo(newEmail, id, newName);
+//        }
 
-        public bool UpdatePassword(string id, string pwd)
-        {
-            var db = new MySqlDatabase();
-            return db.UpdatePassword(pwd, id);
-        }
+//        public bool UpdatePassword(string id, string pwd)
+//        {
+//            var db = new MySqlDatabase();
+//            return db.UpdatePassword(pwd, id);
+//        }
 
-        public UserDetails retieve_user(string userid)
-        {
-            var db = new MySqlDatabase();
-            return db.Get_user(userid);
-        }
+//        public UserDetails retieve_user(string userid)
+//        {
+//            var db = new MySqlDatabase();
+//            return db.Get_user(userid);
+//        }
 
-        public List<Searchlist> retrieve_searches(string userid)
-        {
-            var db = new MySqlDatabase();
-            return db.retrieve_searches_db(userid);
-        }
+//        public List<Searchlist> retrieve_searches(string userid)
+//        {
+//            var db = new MySqlDatabase();
+//            return db.retrieve_searches_db(userid);
+//        }
 
-        public Searchview retrieve_searchview(string userid)
-        {
-            var db = new MySqlDatabase();
-            return db.retrieve_searchview_db(userid);
-        }
-    }
-}
+//        public Searchview retrieve_searchview(string userid)
+//        {
+//            var db = new MySqlDatabase();
+//            return db.retrieve_searchview_db(userid);
+//        }
+//    }
+//}
