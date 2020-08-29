c06f5d8ba5d05281f3acc9375df1379b93643020
CHIRAGH-II <safee.ullah@gmail.com>
2020-08-30 03:09:25 +0500

    Create AddSuffixInName.cs

---
+++ Code/PerceptronAPI/PerceptronAPI/Utility/AddSuffixInName.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+using PerceptronAPI.Engine;
+using PerceptronAPI.Repository;
+
+namespace PerceptronAPI.Utility
+{
+    public class AddSuffixInName
+    {
+        public string AddSuffix(string filename, string suffix) // For adding FileUniqueId before the extension of the file
+        {
+            string FileDirectory = Path.GetDirectoryName(filename);
+            string fName = Path.GetFileNameWithoutExtension(filename);
+            string fExt = Path.GetExtension(filename);
+            return Path.Combine(FileDirectory, String.Concat(fName, suffix, fExt));
+        }
+    }
+}
