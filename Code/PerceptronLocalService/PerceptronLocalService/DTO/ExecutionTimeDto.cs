16c76b763ff94a14c135420416fee19ce326008a
CHIRAGH-II <safee.ullah@gmail.com>
2020-10-22 20:05:59 +0500

    Update ExecutionTimeDto.cs

--- Code/PerceptronLocalService/PerceptronLocalService/DTO/ExecutionTimeDto.cs
+++ Code/PerceptronLocalService/PerceptronLocalService/DTO/ExecutionTimeDto.cs
@@ -2,13 +2,18 @@
 {
     public class ExecutionTimeDto
     {
+        public string FileReadingTime;
+        public string TunerTime;
+        public string PstTime;
+
+
         public string InsilicoTime;
         public string PtmTime;
-        public string TunerTime;
+        
         public string MwFilterTime;
-        public string PstTime;
+        
         public string TotalTime;
-        public string FileReadingTime;
+        
         public string TruncationEngineTime;
     }
 }
