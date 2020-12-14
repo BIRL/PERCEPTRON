1069e49a63a5390304ab713e7157cbfe546d42cd
CHIRAGH-II <safee.ullah@gmail.com>
2020-12-15 02:05:19 +0500

    Create ConvertStringListToDoubleList.cs

---
+++ Code/PerceptronLocalService/PerceptronLocalService/Utility/ConvertStringListToDoubleList.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerceptronLocalService.Utility
+{
+    public class ConvertStringListToDoubleList
+    {
+        public List<double> ConvertStringToDouble(string[] dummySplit)
+        {
+            List<double> newList = new List<double>(dummySplit.Length);
+            double OutDouble = 0.0;
+            for (int i = 0; i < dummySplit.Length; i++)
+            {
+                double.TryParse(dummySplit[i], out OutDouble);
+                newList.Add(OutDouble);
+            }
+            return newList;
+        }
+    }
+}
