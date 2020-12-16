c5f7c6a327bfac1d9034c8c8fa8009212a79c4c6
CHIRAGH-II <safee.ullah@gmail.com>
2020-12-16 19:33:40 +0500

    Create ResultsDownloadToBeWrite.cs

---
+++ Code/PerceptronLocalService/PerceptronLocalService/DTO/ResultsDownloadToBeWrite.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PerceptronLocalService.DTO
+{
+    public class ResultsDownloadToBeWrite
+    {
+        public string FileName;
+        public List<ProteinDto> CandidateList;
+        public ProteinDto Protein;
+        public string Time;
+
+        public ResultsDownloadToBeWrite()
+        {
+            FileName = "";
+            CandidateList = new List<ProteinDto>();
+        }
+
+        public ResultsDownloadToBeWrite(string cFileName, List<ProteinDto> cCandidateList)
+        {
+            FileName = cFileName;
+            CandidateList = cCandidateList;
+        }
+
+
+        public ResultsDownloadToBeWrite(string cFileName, ProteinDto cProtein, string cTime)
+        {
+            FileName = cFileName;
+            Protein = cProtein;
+            Time = cTime;
+        }
+
+    }
+}
