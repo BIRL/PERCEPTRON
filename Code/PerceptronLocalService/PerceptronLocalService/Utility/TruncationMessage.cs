fa2a83d086da4a4efe5a49f030da4f35db0aadcc
CHIRAGH-II <safee.ullah@gmail.com>
2020-12-20 03:47:29 +0500

    Create TruncationMessage.cs

---
+++ Code/PerceptronLocalService/PerceptronLocalService/Utility/TruncationMessage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerceptronLocalService.Utility
+{
+    public class TruncationMessage
+    {
+        public string TypeOfTruncation(string Truncation)
+        {
+            var Truncation_Message = "";
+            if (Truncation == "Left")
+            {
+                Truncation_Message = "Truncation at N-Terminal Side";
+            }
+            else if (Truncation == "Right")
+            {
+                Truncation_Message = "Truncation at C-Terminal Side";
+            }
+            else
+            {
+                Truncation_Message = "No Truncation";
+            }
+            return Truncation_Message;
+        }
+    }
+}
