110181710706d60dea72b0306b2ff4d694b4c575
CHIRAGH-II <safee.ullah@gmail.com>
2020-12-19 16:01:42 +0500

    Created

---
+++ Code/PerceptronLocalService/PerceptronLocalService/Utility/NoOfMatchedFragments.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerceptronLocalService.Utility
+{
+    public class NoOfMatchedFragments
+    {
+        public int NoOfMatchedFragmentsCount(int Matches, List<int> LeftMatchedIndex, List<int> RightMatchedIndex)
+        {
+            if (LeftMatchedIndex.Count != 0)
+            {
+                Matches = LeftMatchedIndex.Count;
+            }
+            else if (RightMatchedIndex.Count != 0)
+            {
+                Matches = Matches + RightMatchedIndex.Count;
+            }
+            return Matches;
+            
+        }
+    }
+}
---
+++ Code/PerceptronLocalService/PerceptronLocalService/Utility/NoOfPtmModifications.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using PerceptronLocalService.DTO;
+
+namespace PerceptronLocalService.Utility
+{
+    public class NoOfPtmModifications
+    {
+        public int NoOfPtmModificationsCount(int NoPtmModificaitons, List<PostTranslationModificationsSiteDto> PtmModifications)
+        {
+            if (PtmModifications.Count != 0)
+            {
+                NoPtmModificaitons = PtmModifications.Count;
+            }
+            return NoPtmModificaitons;
+        }
+    }
+}
