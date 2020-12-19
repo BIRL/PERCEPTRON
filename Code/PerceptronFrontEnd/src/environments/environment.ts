62d44c18bf96cc22bd23140e67e8662f83a6371b
CHIRAGH-II <safee.ullah@gmail.com>
2020-12-20 03:46:52 +0500

    Update environment.ts

--- Code/PerceptronFrontEnd/src/environments/environment.ts
+++ Code/PerceptronFrontEnd/src/environments/environment.ts
@@ -6,11 +6,35 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: "AIzaSyCrdVbt7BrEne5lok4F78wjWxDtLNDcQs4",
-    authDomain: "proteomics-tdp.firebaseapp.com",
-    databaseURL: "https://proteomics-tdp.firebaseio.com",
-    projectId: "proteomics-tdp",
-    storageBucket: "proteomics-tdp.appspot.com",
-    messagingSenderId: "589100138546"
+apiKey: "AIzaSyCqHOjPtqY7MxWRtj-RDW7aI-n8fe1ldmM",
+    authDomain: "perceptron-51a86.firebaseapp.com",
+    databaseURL: "https://perceptron-51a86.firebaseio.com",
+    projectId: "perceptron-51a86",
+    storageBucket: "perceptron-51a86.appspot.com",
+    messagingSenderId: "708562387352",
+    appId: "1:708562387352:web:f685ab2e4474c6c941abed",
+    measurementId: "G-318PE2YSNP"
   }
 };
+
+
+
+
+//New Firebase 
+// apiKey: "AIzaSyCqHOjPtqY7MxWRtj-RDW7aI-n8fe1ldmM",
+//     authDomain: "perceptron-51a86.firebaseapp.com",
+//     databaseURL: "https://perceptron-51a86.firebaseio.com",
+//     projectId: "perceptron-51a86",
+//     storageBucket: "perceptron-51a86.appspot.com",
+//     messagingSenderId: "708562387352",
+//     appId: "1:708562387352:web:f685ab2e4474c6c941abed",
+//     measurementId: "G-318PE2YSNP"
+
+
+// Old Firebase
+// apiKey: "AIzaSyCrdVbt7BrEne5lok4F78wjWxDtLNDcQs4",
+// authDomain: "proteomics-tdp.firebaseapp.com",
+// databaseURL: "https://proteomics-tdp.firebaseio.com",
+// projectId: "proteomics-tdp",
+// storageBucket: "proteomics-tdp.appspot.com",
+// messagingSenderId: "589100138546"
