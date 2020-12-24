39db942dc7c2841814b64ba8240f4a2936a4f495
CHIRAGH-II <safee.ullah@gmail.com>
2020-12-24 19:22:37 +0500

    Updated history.component

--- Code/PerceptronFrontEnd/src/app/history/history.component.ts
+++ Code/PerceptronFrontEnd/src/app/history/history.component.ts
@@ -73,15 +73,13 @@ export class HistoryComponent implements OnInit {
     }
     else if(row.progress == "In Queue" || row.progress == "Running"){
       alert("Dear User,\nYour query is being processed. Please wait, the search may take several minutes to complete. We appreciate your patience!\n\nThank you for using PERCEPTRON!\nThe PERCEPTRON Team");
-
-
-
+    }
+    else if(row.progress == "Result Expired"){
+      alert("Dear User,\nThe link you followed has expired. Please go to 'Protein Search Query' to submit proteoform search query.\n\nThank you for using PERCEPTRON!\nThe PERCEPTRON Team");
     }
     else if(row.progress == "Error in Query")  // It means query wasn't able to complete properly, and there would be an issue into query parameters, Peaklist hadn't reasonable amount of data etc.
     //Therefore, it will not navigate to Scan Results.
     {
-      alert();
-
       if (window.confirm("Dear User,\nWe're sorry, your search query could not be processed because of the reasons indicated below:\nOne or more input parameters are invalid\nMissing information in the data file added\nPlease address these issues to proceed.\n\nIf problem persists, please report your problem here or contact us.\nThank you for using PERCEPTRON!\nThe PERCEPTRON Team"))
       {
         window.location.href='https://github.com/BIRL/PERCEPTRON/issues';
