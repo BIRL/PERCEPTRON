3e5ed064e1cded2dcd0f1f5f12dd9fbbbf85e58d
CHIRAGH-II <safee.ullah@gmail.com>
2020-12-31 03:19:46 +0500

    Update CallingPerceptronAPI.m

--- Code/CallingPerceptronAPI/CallingPerceptronAPI.m
+++ Code/CallingPerceptronAPI/CallingPerceptronAPI.m
@@ -1,25 +1,8 @@
 
-% UniqueID = java.util.UUID.randomUUID.toString;   %% Generate Unique ID so that it would be easy to work around
-try
     ftpobj = ftp('*****', '****', '****');
     %%Password Added here....
-    
-    
-    mput(ftpobj,"HELA_pk13_sw1_66sc_mono.txt");
-    
-    
-    
-catch
-    msgbox('Search couldn''t complete please check your search parameters and data file. If problem still persists report bug on GitHub','CallingPerceptronApi','Modal');
-end
+UniqueUserID = string(java.util.UUID.randomUUID.toString);   %% Generate Unique User ID
 
-import matlab.net.http.*
-
-PerceptronApiUrl = 'http://localhost:52340/api/search/Calling_API';
-
-%JUST CALL CHECKING...
-SendParameters = RequestMessage('POST',[]);
-Options = matlab.net.http.HTTPOptions('ConnectTimeout',1000);  %% 1000s
 %%% ATTENTION!!! ALL PARAMETER VALUES ARE CASE SENSITIVE
 %%% User will add values below...!!!
 %Title
@@ -49,13 +32,77 @@ FilterDb = "True";     % User can also, select "False"
 % ParameterValue = [JobName;FDRCutOff;ProteinDatabase;MassMode;FilterDb;"500";"15";"ppm";"False";"HCD";"bo,bstar,yo,ystar";"True";"3";"6";"0.1";"Da";"0.45";"False"; "None,NME,NME_Acetylation,M_Acetylation";"False";"0.5" ; "";"";"None";"";"None";"0"; "0";"100";"";"";"farhan.khalid@lums.edu.pk"; "farhan.khalid@lums.edu.pk"];
 % JsonString = jsonencode(table(ParameterName,ParameterValue))
 % SendParameters.Body = JsonString;
+
+FullFileName = 'D:\01_PERCEPTRON\gitHub\PERCEPTRON\Code\CallingPerceptronAPI\HELA_pk13_sw1_66sc_mono.txt'; % Please add here the path alongwith filename
+[FilePath,FileName,Extension] = fileparts(FullFileName);
+FileNameWithExtension = strcat(FileName,Extension);
+try
+    mput(ftpobj,FullFileName);
+catch
+    msgbox('Input file couldn''t uploaded at Server please check your internet connection and data file. If problem still persists report bug on GitHub','CallingPerceptronApi','Modal');
+end
+
+import matlab.net.http.*
+%JUST CALL CHECKING...
+SendParameters = RequestMessage('POST',[]);
+Options = matlab.net.http.HTTPOptions('ConnectTimeout',1000);  %% 1000sec
+BaseApiUrl = "http://localhost:52340/";
+PerceptronApiJobSubmissionUrl = strcat(BaseApiUrl,'api/search/Calling_API');
+
 Pattren = ":";
 
-
-
-ParameterValue = strcat(JobName,Pattren,FDRCutOff,Pattren,ProteinDatabase,Pattren,MassMode,Pattren,FilterDb,Pattren,"500",Pattren,"15",Pattren,"ppm",Pattren,"False",Pattren,"HCD",Pattren,"bo,bstar,yo,ystar",Pattren,"True",Pattren,"3",Pattren,"6",Pattren,"0.1",Pattren,"Da",Pattren,"0.45",Pattren,"False",Pattren,"None,NME,NME_Acetylation,M_Acetylation",Pattren,"False",Pattren,"0.5",Pattren,"",Pattren,"",Pattren,"None",Pattren,"None",Pattren,"0",Pattren,"0",Pattren,"100",Pattren,"farhan.khalid@lums.edu.pk",Pattren,"farhan.khalid@lums.edu.pk");
+ParameterValue = strcat(JobName,Pattren,FDRCutOff,Pattren,ProteinDatabase,Pattren,MassMode,Pattren,FilterDb,Pattren,"500",Pattren,"15",Pattren,"ppm",Pattren,"False",Pattren,"HCD",Pattren,"bo,bstar,yo,ystar",Pattren,"True",Pattren,"3",Pattren,"6",Pattren,"0.1",Pattren,"Da",Pattren,"0.45",Pattren,"False",Pattren,"None,NME,NME_Acetylation,M_Acetylation",Pattren,"False",Pattren,"0.5",Pattren,"",Pattren,"",Pattren,"None",Pattren,"None",Pattren,"0",Pattren,"0",Pattren,"100",Pattren,"farhan.khalid@lums.edu.pk",Pattren,UniqueUserID,Pattren,FileNameWithExtension);
 SendParameters.Body = ParameterValue;
-Response = SendParameters.send(PerceptronApiUrl, Options);
+Response = SendParameters.send(PerceptronApiJobSubmissionUrl, Options);
+try
+if (Response.StatusCode == "OK")
+    java.lang.Thread.sleep(600);  %in sec
+    PerceptronApiJobStatusUrl = strcat(BaseApiUrl,'api/search/CallingPerceptronApiHistory');
+    JobStatusResquest = RequestMessage('POST',[]);
+    JobStatusResquest.Body = UniqueUserID;
+    JobStatusReponse = JobStatusResquest.send(PerceptronApiJobStatusUrl, Options);
+    
+    if (JobStatusReponse.Body.Data.progress == "In Queue" || JobStatusReponse.Body.Data.progress == "Running")
+        Wait = true;
+        while(Wait)
+            java.lang.Thread.sleep(600);  %in sec
+            JobStatusReponse = JobStatusResquest.send(PerceptronApiJobStatusUrl, Options);
+            if (JobStatusReponse.Body.Data.progress == "Completed" || JobStatusReponse.Body.Data.progress == "Error in Query" || JobStatusReponse.Body.Data.progress == "Result Expired")
+                Wait = false;
+            end
+        end
+    end
+    
+    if (JobStatusReponse.Body.Data.progress == "Completed")
+        
+        
+    elseif (JobStatusReponse.Body.Data.progress == "Error in Query")
+        msgbox('Dear User, there is an error in your query.','CallingPerceptronApi','Modal');
+        throw;
+    elseif (JobStatusReponse.Body.Data.progress == "Result Expired")
+        msgbox('Dear User, your search results has been expired.','CallingPerceptronApi','Modal');
+        throw;
+    
+    end
+    
+    
+    
+    sdadsad= 1;
+    
+    
+else
+    msgbox('Search couldn''t complete please check your search parameters and data file. If problem still persists report bug on GitHub','CallingPerceptronApi','Modal');
+end
+catch
+    
+end
+
+% Message = char(strcat('Dear User, please save this ID:', UniqueUserID, ' to access your results upto 48hrs.'));
+% msgbox(Message,'CallingPerceptronApi','Modal');
+
+fdsf = 23;
+
+
 
 
 
