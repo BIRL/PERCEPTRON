d0c347ba5173f9c37a1adbf1ab10e062ff00288f
CHIRAGH-II <safee.ullah@gmail.com>
2020-12-31 14:37:59 +0500

    Update admin-panel.component.ts

--- Code/PerceptronFrontEnd/src/app/admin-panel/admin-panel.component.ts
+++ Code/PerceptronFrontEnd/src/app/admin-panel/admin-panel.component.ts
@@ -1,8 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { ConfigService } from '../config.service';
 import { FormControl } from '@angular/forms';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 import { Observable } from 'rxjs/Observable';
+import * as fileSaver from 'file-saver';
+import { formArrayNameProvider } from '@angular/forms/src/directives/reactive_directives/form_group_name';
+import { from } from 'rxjs/observable/from';
 
 
 @Component({
@@ -13,15 +16,17 @@ import { Observable } from 'rxjs/Observable';
 })
 export class AdminPanelComponent implements OnInit {
 
-  NameOfDatabase: any;
-  filenameModel: any;
+  NameOfDatabaseToBeDownloaded: any;
+  filenameModel: boolean = false;
   NameOfDatabaseToBeUpdated: any;
   UploadedFile: any;
+  blob : any;
 
   IsWaitSubmit = 0;
   IsWaitDownload = 0;
   stateCtrl: FormControl;
   filteredStates: Observable<any[]>;
+  @ViewChild("imgFileInput") imgFileInput;
 
   constructor(private route: ActivatedRoute, private router: Router, private _httpService: ConfigService) {
     this.stateCtrl = new FormControl();
@@ -60,19 +65,70 @@ export class AdminPanelComponent implements OnInit {
     { name: 'Ecoli', viewValue: 'Ecoli' }
   ];
 
-  onSubmit(file: any): void {
-    var a = 1;
-  }
+  onSubmit(form: any): void {
+    
+    if (this.filenameModel) {
 
+      let fi = this.imgFileInput.nativeElement;
+      let stats: any = 'false';
+      console.log(form);
+      stats = this._httpService.postDatabase(form, fi.files);
+      if (stats == 'success'){
+        alert("Database successfully updated.");
+      }
+      console.log(stats)
+    }
+  }
+  
   onDownload(form: any): void {
-    this.IsWaitDownload = 1;
-    alert("Dear! User your database is successfully downloaded.");
-    this.IsWaitDownload = 0;
+
+    let daf = 1;
+    if (form.NameOfDatabaseToBeDownloaded) {
+
+      this.route.params.subscribe((params: Params) => this.NameOfDatabaseToBeDownloaded = params['NameOfDatabaseToBeDownloaded']);
+    this._httpService.GetResultsDownload(this.NameOfDatabaseToBeDownloaded).subscribe(ResultsData => this.whatResults(ResultsData));
+
+      this.IsWaitDownload = 1;
+      alert("Dear! User your database is successfully downloaded.");
+      this.IsWaitDownload = 0;
+    }
+
+    
+    
+  }
+
+  whatResults(ResultsData: any) {
+
+    //See it later just for #Know
+    //var Data = this.sanitizer.bypassSecurityTrustUrl('data:text/plain;base64,' + filedata);
+
+    for (let i = 0; i < ResultsData.ListOfFileBlobs.length; i++) {  //#ENNT: Can be Removed...
+      let FileName = ResultsData.ZipFileWithPath;
+      let CheckFileType = FileName.split('.').pop();
+      let IndividualResultsFile = ResultsData.ListOfFileBlobs[i];
+
+      const byteCharacters = atob(IndividualResultsFile);
+      const byteNumbers = new Array(byteCharacters.length);
+      for (let i = 0; i < byteCharacters.length; i++) {
+        byteNumbers[i] = byteCharacters.charCodeAt(i);
+      }
+      const byteArray = new Uint8Array(byteNumbers);
+      this.blob = new Blob([byteArray], { type: "application/zip;charset=utf-8" })//"text/plain;charset=utf-8" });
+
+      fileSaver.saveAs(this.blob, FileName);
+
+
+      console.log(FileName + "Successfully Downloaded!!!");
+
+    }
+    //alert("Dear User Your Result File(s) Downloaded. \nPlease See Your Download Folder.");
+
+
   }
 
 
-  upload(UploadedFile: any): void {
-
+  upload(form: any): void {
+    let test = 1;
   }
   // let fi = this.imgFileInput.nativeElement;
   //   let stats: any = 'false';
