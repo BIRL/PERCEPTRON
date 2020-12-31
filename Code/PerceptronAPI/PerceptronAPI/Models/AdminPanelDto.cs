048bcb8a53347285bfb11361f44d2df0d1157e9e
CHIRAGH-II <safee.ullah@gmail.com>
2020-12-31 14:37:29 +0500

    Create AdminPanelDto.cs

---
+++ Code/PerceptronAPI/PerceptronAPI/Models/AdminPanelDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PerceptronAPI.Models
+{
+    public class AdminPanelDto
+    {
+        public string UpdateDatabase { get; set; }
+        public string DownloadDatabase { get; set; }
+        public string FileName { get; set; }
+    }
+}
