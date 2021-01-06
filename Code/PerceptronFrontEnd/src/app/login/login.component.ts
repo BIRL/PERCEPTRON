08487f16eff2d0f8be78760b53d4ad191703ad24
CHIRAGH-II <safee.ullah@gmail.com>
2021-01-06 20:51:52 +0500

    Update login.component.ts

--- Code/PerceptronFrontEnd/src/app/login/login.component.ts
+++ Code/PerceptronFrontEnd/src/app/login/login.component.ts
@@ -3,9 +3,9 @@ import { AngularFireAuth } from 'angularfire2/auth';
 import { Router } from '@angular/router';
 import { moveIn } from '../router.animations';
 import * as firebase from 'firebase/app';
-import {AppComponent} from '../app.component';
+import { AppComponent } from '../app.component';
 import { Output, EventEmitter } from '@angular/core';
-import {MatToolbarModule, MatSidenavModule, MatCardModule, MatButtonModule, MatIconModule} from '@angular/material';
+import { MatToolbarModule, MatSidenavModule, MatCardModule, MatButtonModule, MatIconModule } from '@angular/material';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
@@ -19,15 +19,15 @@ export class LoginComponent implements OnInit {
   constructor(public myapp: AppComponent, public af: AngularFireAuth, private router: Router) {
     this.af.authState.subscribe(user => { })
   }
-  
-  
+
+
 
   loginFb() {
     var provider = new firebase.auth.FacebookAuthProvider();
     provider.addScope('email');
-    var  x = this.router;
+    var x = this.router;
     var login = firebase.auth().signInWithPopup(provider);
-    login.then(function(result) {
+    login.then(function (result) {
       // This gives you a Facebook Access Token.
       // var token = result.credential.accessToken;
       // The signed-in user info.
@@ -37,18 +37,22 @@ export class LoginComponent implements OnInit {
       (err) => {
         alert(err);
         this.error = err;
-      })     
+      })
   };
 
   loginGoogle() {
     var provider = new firebase.auth.GoogleAuthProvider();
-    var  x = this.router;
+    var x = this.router;
     firebase.auth().signInWithPopup(provider).then(function (result) {
       // This gives you a Google Access Token.
       // var token = result.credential.accessToken;
       // The signed-in user info.
       // var user = result.user;
-      x.navigate(['/search']);
+      localStorage.removeItem('login');
+      localStorage.removeItem('logged_in_user');
+      alert("Dear User,\nYour account has been successfully created. Please login with your credentials.\n\nThank you for using PERCEPTRON!\nThe PERCEPTRON Team");
+
+      x.navigate(['/login']);
     }).catch(
       (err) => {
         this.error = err;
@@ -59,31 +63,31 @@ export class LoginComponent implements OnInit {
     var x = this.router;
     var y = this.myapp;
     firebase.auth().signInAnonymously().then(function (result) {
-      localStorage.setItem('login','1');
-      localStorage.setItem('logged_in_user','guest')
+      localStorage.setItem('login', '1');
+      localStorage.setItem('logged_in_user', 'guest')
       y.disabled = true;
       y.logged_in_user = localStorage.getItem('logged_in_user')
       y.disabled1 = false;
-       x.navigate(['/search']);
-     }).catch(
-       (err) => {
-         this.error = err;
-         alert(err);
-       })
+      x.navigate(['/search']);
+    }).catch(
+      (err) => {
+        this.error = err;
+        alert(err);
+      })
 
-       
+
   };
- 
+
 
   ngOnInit() {
-    this.myapp.disabled=true;
+    this.myapp.disabled = true;
   }
-  ngOnDestroy(){
-    if (this.myapp.disabled1){
-      this.myapp.disabled=false;
+  ngOnDestroy() {
+    if (this.myapp.disabled1) {
+      this.myapp.disabled = false;
     }
-    else{
-      this.myapp.disabled=true;
+    else {
+      this.myapp.disabled = true;
     }
-    }
+  }
 }
