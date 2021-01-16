6382ab569701f22e038e3d367717d9775467968d
CHIRAGH-I <safee.ullah@gmail.com>
2021-01-16 16:26:52 +0500

    Create TruncationIndexed.cs

---
+++ Code/PerceptronLocalService/PerceptronLocalService/Utility/TruncationIndexed.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerceptronLocalService.Utility
+{
+    public class TruncationIndexed  //C# is Zero Indexed so this class is for removing Zero Indexing by adding 1
+    {
+        public int TruncationIndxing(int TruncationIndex)
+        {
+            if (TruncationIndex != -1)
+            {
+                TruncationIndex = TruncationIndex + 1;
+            }
+
+            return TruncationIndex;
+        }
+    }
+}
