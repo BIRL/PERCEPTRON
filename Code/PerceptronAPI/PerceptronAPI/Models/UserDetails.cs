7f279e1974b396eef743604fc5a18e677561847d
CHIRAGH-I <safee.ullah@gmail.com>
2021-01-23 20:28:29 +0500

    Revert "Update UserDetails.cs"
    This reverts commit 5f90e119d1a52660c67634cf00df020681608981.

--- Code/PerceptronAPI/PerceptronAPI/Models/UserDetails.cs
+++ Code/PerceptronAPI/PerceptronAPI/Models/UserDetails.cs
@@ -2,37 +2,9 @@
 {
     public class UserDetails
     {
-        public string UserName { get; set; }
-        public string Password { get; set; }
-        private static string Error;
-
-        public UserDetails(string cUserName, string cPassword)
-        {
-            UserName = cUserName;
-            Password = cPassword;
-        }
-
-        public static bool IsEqual(UserDetails user1, UserDetails user2)    // user1 -  Database save User          user2 - logged in user         // Authenticate User
-        {
-
-            if (user1 == null || user2 == null)
-            {
-                return false;
-            }
-            else
-            {
-                if (user1.UserName != user2.UserName)
-                {
-                    Error = "Username does not exist!";     // Error = "Username and/or password does not exist!";
-                    return false;
-                }
-                else if (user1.Password != user2.Password)
-                {
-                    Error = "Username and/or password does not exist!";
-                    return false;
-                }
-                return true;
-            }
-        }
+        public string UName;
+        public string Name;
+        public string Email;
+        public string RPass;
     }
 }
