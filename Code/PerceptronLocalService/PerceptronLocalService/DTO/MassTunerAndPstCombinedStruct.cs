bc68a714dded24ba65799472a2a61f287b6d776f
CHIRAGH-I <safee.ullah@gmail.com>
2021-03-15 16:47:07 +0500

    Updated

---
+++ Code/PerceptronLocalService/PerceptronLocalService/DTO/MassTunerAndPstCombinedStruct.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerceptronLocalService.DTO
+{
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
+    public struct MassTunerAndPstCombinedStruct
+    {
+        public int PstTagLength;
+        public double PstErrorScore;
+        public double PstFrequency;
+        public double MassTuner;
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+        public char[] PstTags;
+        //public MassTunerAndPstCombinedStruct (int PstTagLength, char[] PstTags, double PstErrorScore, double PstFrequency, double MassTuner)
+        //{
+        //    this.PstTagLength = PstTagLength;
+        //    this.PstTags = PstTags;
+        //    this.PstErrorScore = PstErrorScore;
+        //    this.PstFrequency = PstFrequency;
+        //    this.MassTuner = MassTuner;
+        //}
+    }
+}
