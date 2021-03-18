38a214f483cc1a5bead0a35f9122e0696a8ec237
CHIRAGH-I <safee.ullah@gmail.com>
2021-03-18 22:33:34 +0500

    Create PerceptronSdkResults.cs

---
+++ Code/PerceptronLocalService/PerceptronLocalService/DTO/PerceptronSdkResults.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PerceptronLocalService.DTO
+{
+    public class PerceptronSdkResults
+    {
+        public string QueryId { get; set; }
+        public string Title { get; set; }
+        public string UserName { get; set; }
+        public string ResultsAvailable { get; set; }
+
+        //public PerceptronSdkResults(DateTime cJobSubmission, string cQueryId, string cTitle, string cUserName, string cResultsAvailable)
+        //{
+        //    JobSubmission = cJobSubmission;
+        //    QueryId = cQueryId;
+        //    Title = cTitle;
+        //    UserName = cUserName;
+        //    ResultsAvailable = cResultsAvailable;
+        //}
+
+    }
+}
