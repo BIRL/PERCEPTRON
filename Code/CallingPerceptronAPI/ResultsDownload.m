305d957df8d3d055b005450072f0564bc291c654
CHIRAGH-I <safee.ullah@gmail.com>
2021-03-21 13:02:08 +0500

    Update ResultsDownload.m

--- Code/CallingPerceptronAPI/ResultsDownload.m
+++ Code/CallingPerceptronAPI/ResultsDownload.m
@@ -1,21 +1,20 @@
-function Message = ResultsDownload( BaseApiUrl, UserName, EmailAddress, Password )
+function Message = ResultsDownload( BaseApiUrl, FtpServerName, FtpUserName, UserName, EmailAddress, Password )
 
 %Dear User, Result file would be zip file with the name of User's job title
-% Also, if two queries 
 
-CredentialInfo = importdata("C:\01_DoNotEnterPerceptronRelaventInfo\PerceptronFtpInfo.txt");
-FtpServerName =  char(CredentialInfo(1,1)); %%string (CredentialInfo.textdata{1, 1});% + string(CredentialInfo.data);
 ResultFileName = char("Default Run" + ".zip");
 
 ResultFilesFolder = char(pwd + "\ResultFiles\");
 
 try
-    ftpobj = ftp(FtpServerName, UserName, Password);
+    ftpobj = ftp(FtpServerName, FtpUserName, Password);
+    cd(ftpobj);  sf=struct(ftpobj);  sf.jobject.enterLocalPassiveMode();
     mget(ftpobj,ResultFileName, ResultFilesFolder);
 catch
     Message = "File is unable to download on server."
     msgbox('Input file couldn''t downloaded at Server please check your internet connection and data file. If problem still persists report bug on GitHub','CallingPerceptronApi','Modal');
     return;
 end
+Message = "File Successfully downloaded and available at provided path."
 end
 
