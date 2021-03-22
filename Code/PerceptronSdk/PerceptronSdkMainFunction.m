3600a190594cb1bd90fe1e953c4ce0d854fcacf1
CHIRAGH-I <safee.ullah@gmail.com>
2021-03-22 14:45:45 +0500

    Update PerceptronSdkMainFunction.m

--- Code/PerceptronSdk/PerceptronSdkMainFunction.m
+++ Code/PerceptronSdk/PerceptronSdkMainFunction.m
@@ -2,40 +2,32 @@ function [] = MainCallingPerceptronApi( )
 
 BaseApiUrl = "https://perceptron.lums.edu.pk/PerceptronAPI/"; %%  
 
-UserName = "Farhan";
-EmailAddress = "farhan.biomedical.2022@gmail.com";
-Password = char(string(importdata("C:\01_DoNotEnterPerceptronRelaventInfo\FarhanCredentials.txt")));
-
 %%% For Guest User Credential  
 % UserName = "Guest";
 % EmailAddress = "Guest";    % User can add here email address, if user wants to receive an email once results are available
-% DummyPassword = "WelcomePerceptron";
+% Password = "WelcomePerceptron";
 
+try
 %% Use RegisterUser for signup, only needed to execute for one time
-%Message = RegisterUser( BaseApiUrl, UserName,  EmailAddress, DummyPassword);
+%Message = RegisterUser( BaseApiUrl, UserName,  EmailAddress, Password);
 
 %% Use VerfiyingEmailAddress for email verification, only needed to execute for one time
-%Message = VerfiyingEmailAddress( BaseApiUrl,  UserName, EmailAddress, DummyPassword, UserUniqueId);
-
+%Message = VerfiyingEmailAddress( BaseApiUrl,  UserName, EmailAddress, Password, UserUniqueId);
 
 FtpServerName = 'perceptron.lums.edu.pk';
 FtpUserName =  char("perceptron.lums.edu.pk" + "|" + UserName) ;
-FullFileName = 'HELA_pk13_sw1_66sc_mono.txt'; % Please add here the path alongwith filename
-
-
-%% For LocalHost Testing below is the code
-BaseApiUrl = "http://localhost:52340/";
-FtpServerName = char(string(importdata("C:\01_DoNotEnterPerceptronRelaventInfo\PerceptronFtpInfo.txt")) + (":21"));
-FtpUserName =  char(UserName);
-
+FullFileName = 'HELA_pk13_sw1_66sc_mono.txt'; % Please add here the complete path of your input file
 
 
 %% For Submitting query and uploading file on FTP
-Message = SearchQuery( BaseApiUrl, FtpServerName, FtpUserName, UserName, EmailAddress, Password, FullFileName )
+%Message = SearchQuery( BaseApiUrl, FtpServerName, FtpUserName, UserName, EmailAddress, Password, FullFileName )
 
 %% For downloading results
-Message = ResultsDownload( BaseApiUrl, FtpServerName, FtpUserName, UserName, EmailAddress, Password )
+%Message = ResultsDownload( BaseApiUrl, FtpServerName, FtpUserName, UserName, EmailAddress, Password )
 
 %% Messages will be displayed here
 disp (Message)
+catch exception
+    msgbox ('Warning: Something went wrong. If problem still persists please report an issue on GitHub.','PerceptronSdk','Modal');
+end
 end
