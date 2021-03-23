9df9c09ab1e002b910f04cad8516f1d5447266cd
CHIRAGH-I <safee.ullah@gmail.com>
2021-03-23 18:20:42 +0500

    Updated

---
+++ Code/PerceptronLocalService/PerceptronLocalService/Utility/AssignPtrToArray.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices;
+using PerceptronLocalService;
+
+namespace PerceptronLocalService.Utility
+{
+    public class AssignPtrToArray
+    {
+        public IntPtr ArrayPtr(List<double> InsilicoList)
+        {
+            var InsilicoArray = new double[] { };
+            int size = 1;
+            IntPtr InsilicoArrayPtr = Marshal.AllocHGlobal(size); //Test me
+            Marshal.Copy(InsilicoArray, 0, InsilicoArrayPtr, InsilicoArray.Length);
+
+            if (InsilicoList.Count != 0)
+            {
+                InsilicoArray = InsilicoList.ToArray();
+                size = Marshal.SizeOf(InsilicoArray[0]) * InsilicoArray.Length;
+                InsilicoArrayPtr = Marshal.AllocHGlobal(size);
+                Marshal.Copy(InsilicoArray, 0, InsilicoArrayPtr, InsilicoArray.Length);
+            }
+            return InsilicoArrayPtr;
+        }
+    }
+}
