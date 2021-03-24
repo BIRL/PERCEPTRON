002eee15f9975e07c0d32b3c4b6d1da46729a839
CHIRAGH-I <safee.ullah@gmail.com>
2021-03-24 18:26:03 +0500

    Updated PerceptronSdk

--- Code/PerceptronSdk/PerceptronSdkMainFunction.m
+++ Code/PerceptronSdk/PerceptronSdkMainFunction.m
@@ -1,7 +1,5 @@
 function [] = MainCallingPerceptronApi( )
-
 BaseApiUrl = "https://perceptron.lums.edu.pk/PerceptronAPI/"; %%  
-
 %%% For Guest User Credential  
 % UserName = "Guest";
 % EmailAddress = "Guest";    % User can add here email address, if user wants to receive an email once results are available
@@ -12,19 +10,22 @@ try
 %Message = RegisterUser( BaseApiUrl, UserName,  EmailAddress, Password);
 
 %% Use VerfiyingEmailAddress for email verification, only needed to execute for one time
-%Message = VerfiyingEmailAddress( BaseApiUrl,  UserName, EmailAddress, Password);
+% Dear User please paste below the line of UserUniqueId = 'XXX-YYY21-ZZZZ-YZXZ-YZYZYZYZY' 
+% as you acquired from the email having subject title is 
+% Calling PERCEPTRON API: Email Verification
 
+% UserUniqueId = 'XXX-YYY21-ZZZZ-YZXZ-YZYZYZYZY'; Please insert here your
+% User's Unique id for email verification.
+%Message = VerfiyingEmailAddress( BaseApiUrl,  UserName, EmailAddress, Password, UserUniqueId);
 FtpServerName = 'perceptron.lums.edu.pk';
 FtpUserName =  char("perceptron.lums.edu.pk" + "|" + UserName) ;
 FullFileName = 'HELA_pk13_sw1_66sc_mono.txt'; % Please add here the complete path of your input file
-
 
 %% For Submitting query and uploading file on FTP
 %Message = SearchQuery( BaseApiUrl, FtpServerName, FtpUserName, UserName, EmailAddress, Password, FullFileName )
 
 %% For downloading results
 %Message = ResultsDownload( BaseApiUrl, FtpServerName, FtpUserName, UserName, EmailAddress, Password )
-
 %% Messages will be displayed here
 disp (Message)
 catch exception
--- Code/PerceptronSdk/VerfiyingEmailAddress.m
+++ Code/PerceptronSdk/VerfiyingEmailAddress.m
@@ -1,19 +1,11 @@
-function Message = VerfiyingEmailAddress( BaseApiUrl,  UserName, EmailAddress, Password)
+function Message = VerfiyingEmailAddress( BaseApiUrl,  UserName, EmailAddress, Password, UserUniqueId)
 
 % Use this function for Email Verfification (after the successfully execution of RegisterUser.m)
-
-% Dear User please paste below the line of UserUniqueId =
-% 'XXX-YYY21-ZZZZ-YZXZ-YZYZYZYZY' as you acquired from the email having subject title is 
-% Calling PERCEPTRON API: Email Verification
-
-% UserUniqueId = 'XXX-YYY21-ZZZZ-YZXZ-YZYZYZYZY'; Please insert here your
-% User's Unique id for email verification.
 
 import matlab.net.http.*
 SendUserInfoForVerify = RequestMessage('POST',[]);
 
-PerceptronApiRegisterUserUrl =  BaseApiUrl + 'api/user/CallingPerceptronApi_VerfiyingEmailAddress' %   CallingPerceptronApiRegisterUser'
-
+PerceptronApiRegisterUserUrl =  BaseApiUrl + 'api/user/CallingPerceptronApi_VerfiyingEmailAddress';
 
 f = ':'
 
