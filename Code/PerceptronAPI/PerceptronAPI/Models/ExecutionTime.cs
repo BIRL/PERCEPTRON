5c1a5ce12612551846cba543f71de13ef1f14f29
CHIRAGH-I <safee.ullah@gmail.com>
2021-03-31 15:59:41 +0500

    Updated PerceptronEdmx

--- Code/PerceptronAPI/PerceptronAPI/Models/ExecutionTime.cs
+++ Code/PerceptronAPI/PerceptronAPI/Models/ExecutionTime.cs
@@ -14,14 +14,15 @@ namespace PerceptronAPI.Models
     
     public partial class ExecutionTime
     {
+        public int FileId { get; set; }
         public System.DateTime JobSubmission { get; set; }
+        public string QueryId { get; set; }
         public string InsilicoTime { get; set; }
         public string PtmTime { get; set; }
         public string TunerTime { get; set; }
         public string MwFilterTime { get; set; }
         public string PstTime { get; set; }
         public string TotalTime { get; set; }
-        public string QueryId { get; set; }
         public string FileName { get; set; }
         public string TruncationEngineTime { get; set; }
     }
