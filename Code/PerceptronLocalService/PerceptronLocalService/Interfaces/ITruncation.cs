08c28c5a93507202d5d3d45e7aa17a2c8e745088
imeMFK01 <45854202+imeMFK01@users.noreply.github.com>
2021-04-09 19:14:28 +0500

    Update ITruncation.cs

--- Code/PerceptronLocalService/PerceptronLocalService/Interfaces/ITruncation.cs
+++ Code/PerceptronLocalService/PerceptronLocalService/Interfaces/ITruncation.cs
@@ -9,11 +9,15 @@ namespace PerceptronLocalService.Interfaces
 {
     public interface ITruncation
     {
-        void PreTruncation(double IntactProteinMass, double MwTolerance, List<string> IndividualModifications, List<ProteinDto> CandidateProteinListTruncated, List<ProteinDto> CandidateProteinListTruncatedLeft, List<ProteinDto> CandidateProteinListTruncatedRight, List<newMsPeaksDto> peakData2DList);
+        void PreTruncation(double ProteinExperimentalMw, double MwTolerance, List<string> IndividualModifications, 
+            List<ProteinDto> CandidateProteinListTruncated, List<ProteinDto> CandidateProteinListTruncatedLeft, 
+            List<ProteinDto> CandidateProteinListTruncatedRight, List<newMsPeaksDto> peakData2DList);
 
-        void TruncationLeft(double IntactProteinMass, string PtmAllow, List<ProteinDto> CandidateProteinListTruncatedLeft, List<ProteinDto> CandidateListTruncationLeftProcessed, List<ProteinDto> RemainingProteinsLeft, List<newMsPeaksDto> peakData2DList);
+        void TruncationLeft(double ProteinExperimentalMw, string PtmAllow, List<ProteinDto> CandidateProteinListTruncatedLeft, 
+            List<ProteinDto> CandidateListTruncationLeftProcessed, List<ProteinDto> RemainingProteinsLeft, List<newMsPeaksDto> peakData2DList);
 
-        void TruncationRight(double IntactProteinMass, string PtmAllow, List<ProteinDto> CandidateProteinListTruncatedRight, List<ProteinDto> CandidateListTruncationRightProcessed, List<ProteinDto> RemainingProteinsRight, List<newMsPeaksDto> peakData2DList);
+        void TruncationRight(double ProteinExperimentalMw, string PtmAllow, List<ProteinDto> CandidateProteinListTruncatedRight, 
+            List<ProteinDto> CandidateListTruncationRightProcessed, List<ProteinDto> RemainingProteinsRight, List<newMsPeaksDto> peakData2DList);
 
         List<ProteinDto> FilterTruncatedProteins(SearchParametersDto parameters, List<ProteinDto> CandidateProteinListUnModified, List<PstTagList> PstTags);
     }
