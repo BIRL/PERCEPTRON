4c85a2845d36083723144ef9117d312c95978abb
imeMFK01 <45854202+imeMFK01@users.noreply.github.com>
2021-04-10 14:54:50 +0500

    Update BlindPTMDto.cs

--- Code/PerceptronLocalService/PerceptronLocalService/DTO/BlindPTMDto.cs
+++ Code/PerceptronLocalService/PerceptronLocalService/DTO/BlindPTMDto.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 
 namespace PerceptronLocalService.DTO
 {
@@ -13,6 +9,8 @@ namespace PerceptronLocalService.DTO
         public List<char> HopInfoAA = new List<char>();
         public List<double> HopInfoEnd = new List<double>();
         public List<double> HopInfoStart = new List<double>();
+
+        public BlindPTMDto() { }
 
         public BlindPTMDto(int csizeHopInfo, List<string> cHopInfoName, List<char> cHopInfoAA, List<double> cHopInfoEnd, List<double> cHopInfoStart)
         {
